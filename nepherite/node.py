7bce1a80c4a35b015e86585528d1fb50657e8ac8
Jorge Alejandro Jiménez Luna <jorgeajimenezl17@gmail.com>
2024-04-13 23:02:08 +0200

    Add push transaction to the network

--- nepherite/node.py
+++ nepherite/node.py
@@ -17,7 +17,8 @@ class Utxo:
 
 @payload_dataclass(msg_id=1)
 class Transaction:
-    txin: list[Utxo]
+    timestamp: int
+    input: list[Utxo]
     output: list[Utxo]
 
 
@@ -31,10 +32,13 @@ class BlockHeader:
     difficulty: int
     nonce: int
 
+
 @payload_dataclass(msg_id=3)
 class Block:
     header: BlockHeader
     transactions: list[Transaction]
+    msg_response: int
+
 
 @payload_dataclass(msg_id=4)
 class PullBlockRequest:
@@ -60,30 +64,53 @@ class NepheriteNode(Blockchain):
 
         self.add_message_handler(BlockHeader, self.on_block_header)
         self.add_message_handler(PullBlockRequest, self.on_pull_block_request)
+        self.add_message_handler(Transaction, self.on_transaction)
 
     def get_block_hash(self, header: BlockHeader) -> bytes:
         blob = self.serializer.pack_serializable(header)
         return sha256(blob)
-    
+
     def save_block(self, block: Block) -> bool:
         blob = self.serializer.pack_serializable(block)
         with open(f"data/blocks/{block.seq_num}", "wb") as f:
             f.write(blob)
         return True
-    
+
     def load_block(self, seq_num: int) -> Block:
         with open(f"data/blocks/{seq_num}", "rb") as f:
             blob = f.read()
         return self.serializer.unpack_serializable(blob, Block)
+
+    def verify_transaction(self, transaction: Transaction) -> bool:
+        sum_in = 0
+        sum_out = 0
+        for utxo in transaction.input:
+            sum_in += utxo.amount
+            if utxo.address not in self.chainstate:
+                return False
+        for utxo in transaction.output:
+            sum_out += utxo.amount
+
+        # TODO: fee stuff (ask teacher)
+        return sum_in >= sum_out
+
+    def on_transaction(self, peer: Peer, transaction: Transaction) -> None:
+        if not self.verify_transaction(transaction):
+            return
+        
+        self.mempool.append(transaction)
+        # broadcast transaction
+        for u in self.get_peers():
+            self.ez_send(u, transaction)
 
     @message_wrapper(BlockHeader)
     def on_block_header(self, peer: Peer, header: BlockHeader) -> None:
         block_hash = self.get_block_hash(header)
         if block_hash in self.blocks:
             return
-        
+
         # TODO: Update values and validate
-        
+
     @message_wrapper(PullBlockRequest)
     def on_pull_block_request(self, peer: Peer, request: PullBlockRequest) -> None:
         block_hash = request.block_hash
