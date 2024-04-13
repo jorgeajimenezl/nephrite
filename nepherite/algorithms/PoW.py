45f0596637df68e4925b587717ab03550dbbada6
Mariano Jason <mjasonrc@gmail.com>
2024-04-13 18:44:49 +0200

    Changed :pear: puzzle

--- nepherite/algorithms/PoW.py
+++ nepherite/algorithms/PoW.py
@@ -1,35 +1,23 @@
-'''
-Idea:
-    given the transaction hashes as Th1,..., Thn
-    and the block hash Bh, the PoW will consist on
-    find a solution to a linear equation system described as:
-    
-    y = Ax
+import hashlib
+from typing import Tuple
 
-    with y = Bh, A = [W  ..] , W = [Th1, ..., Thn]
-                     [.. ..]
+# find nonce, such as hash(meessage + nonce ) start with dificulty 0s
+
+# difuculty 5: 1.25 seconds
+# difuculty 6: 11.07 seconds
+# difuculty 7: 38.09 seconds
+
+def solve_puzzle(message: str, difficulty: int) -> Tuple[int, str]:
+    nonce = 0
+    prefix = '0' * difficulty
+    while True:
+        # Concatenate the message with the nonce
+        data = message + str(nonce)
         
-    x any vector that is solution to the equation, A a matrix with W as submatriz 
-    but only at the beggining of it.
-
-
-    easy to check , only multiply x and A that retuns y, and that A containg
-    W in the upper left part.
-'''
-import numpy as np
-
-def linear_equation_check(y: np.ndarray, A: np.ndarray, x: np.ndarray) -> bool:
-    try:
-        can_pass = len(A.shape) == 2 and \
-                len(y.shape) == 1 and \
-                len(x.shape) == 1 and \
-                A.shape[0] == y.shape[0] and \
-                A.shape[1] == x.shape[0]
-
-        if not can_pass:
-            return False
-
-        y_hat = A @ x
-        return np.array_equal(y, y_hat)
-    except:
-        return False
+        hash_value = hashlib.sha256(data.encode()).hexdigest()
+        
+        # Check if the hash value meets the difficulty requirement
+        if hash_value.startswith(prefix):
+            return nonce, hash_value
+        
+        nonce += 1
