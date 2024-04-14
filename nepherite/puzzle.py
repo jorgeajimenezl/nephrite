6c1b1a2acfeadacf6e51324b5307a7bba491f73d
Mariano Jason <mjasonrc@gmail.com>
2024-04-14 14:39:06 +0200

    Added puzzle

--- nepherite/puzzle.py
+++ nepherite/puzzle.py
@@ -1,12 +1,28 @@
-from typing import Generic, TypeVar
+from typing import Tuple
+import hashlib
 
-Answer = TypeVar("Answer")
+Answer = Tuple[int, str]
+DIFFICULTY = 5
 
-class Puzzle(Generic[Answer]):
+class Puzzle:
     @staticmethod
     def compute(data: bytes) -> Answer:
-        pass
+        nonce = 0
+        prefix = '0' * DIFFICULTY
+        while True:
+            data_app = data + nonce.to_bytes(4, byteorder='big')  # Use 4 bytes for nonce
+            
+            hash_value = hashlib.sha256(data_app).hexdigest()
+            
+            if hash_value.startswith(prefix):
+                return nonce, hash_value
+            
+            nonce += 1
     
     @staticmethod
     def verify(data: bytes, answer: Answer) -> bool:
-        pass
+        nonce, hash_value = answer
+        prefix = '0' * DIFFICULTY
+        data_app = data + nonce.to_bytes(4, byteorder='big')  # Use 4 bytes for nonce
+        computed_hash = hashlib.sha256(data_app).hexdigest()
+        return computed_hash.startswith(prefix)
