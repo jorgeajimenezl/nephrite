435d8c172ebeac6649a9bdf5485fd6c8771ce28b
fvaldes0109 <fvaldes0109@gmail.com>
2024-04-19 15:33:05 +0200

    Add tests for node messages

--- tests/unit/test_node.py
+++ tests/unit/test_node.py
@@ -1,5 +1,55 @@
 from ipv8.test.base import TestBase
 
+from nepherite.node import NepheriteNode, TxOut, TransactionPayload, Transaction, Block, PullBlockRequest
 
 class NepheriteNodeTests(TestBase):
-    pass
+    
+    async def test_node_messages(self):
+        """
+        Test that 2 nodes started correctly and are able to send messages to eachother.
+        """
+        self.initialize(NepheriteNode, 2)
+            
+        await self.introduce_nodes()
+        
+        peer_0 = self.nodes[0].my_peer
+        peer_1 = self.nodes[1].my_peer
+
+        # Check if the nodes are connected to eachother
+        self.assertTrue(self.overlay(0).get_peers()[0].mid == peer_1.mid)
+        self.assertTrue(self.overlay(1).get_peers()[0].mid == peer_0.mid)
+        
+        # Arrange a transaction
+        tx_out = TxOut(peer_0.mid, 75)
+        tx = self.overlay(0).make_and_sign_transaction([tx_out])
+        
+        # Send the transaction from node 0 to node 1 and assert that it was received
+        with self.assertReceivedBy(1, [Transaction]) as received_messages:
+            self.overlay(0).ez_send(peer_1, tx)
+            await self.deliver_messages()
+
+        # Assert that the received transaction is the same as the sent transaction
+        self.assertEqual(tx, received_messages[0])
+        
+        # Arrange a block
+        block = self.overlay(0).mine_block()
+
+        # Send the block from node 0 to node 1 and assert that it was received
+        with self.assertReceivedBy(1, [Block]) as received_messages:
+            self.overlay(0).ez_send(peer_1, block)
+            await self.deliver_messages()
+            
+        # Assert that the received block is the same as the sent block
+        self.assertEqual(block, received_messages[0])
+        
+        # Arrange a pull block request
+        pull_block_request = PullBlockRequest(block.header.merkle_root_hash)
+        
+        # Send the pull block request from node 0 to node 1 and assert that it was received
+        with self.assertReceivedBy(1, [PullBlockRequest]) as received_messages:
+            self.overlay(0).ez_send(peer_1, pull_block_request)
+            await self.deliver_messages()
+            
+        # Assert that the received pull block request is the same as the sent pull block request
+        self.assertEqual(pull_block_request, received_messages[0])
+
