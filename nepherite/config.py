2db1e63ab00d93c55134dfb0ddff91000fbcb818
Jorge Alejandro Jiménez Luna <jorgeajimenezl17@gmail.com>
2024-04-26 09:59:28 +0200

    Changed the TTL to 3

--- nepherite/config.py
+++ nepherite/config.py
@@ -3,6 +3,6 @@ import os
 BLOCK_SIZE = os.getenv("BLOCK_SIZE", 4)
 BLOCK_REWARD = os.getenv("BLOCK_REWARD", 100)
 RETRY_COUNT = os.getenv("RETRY_COUNT", 3)
-BLOCK_TTL = os.getenv("BLOCK_TTL", 1)
+BLOCK_TTL = os.getenv("BLOCK_TTL", 3)
 CHAIN_GAP_SIZE = os.getenv("CHAIN_GAP_SIZE", 1)
 SHIBA_RATE = os.getenv("SHIBA_RATE", 2)
